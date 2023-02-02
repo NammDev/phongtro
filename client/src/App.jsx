361a904068cc570361eed70eda0bc7497aba0f98
Nam <namdeveloper.ca@gmail.com>
2023-02-02 09:19:54 +0700

    Change to jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home, Login } from './containers/Public'
+import Header from './containers/components/Header'
+import Navi from './containers/components/Navi'
+
+function App() {
+  return (
+    <Router>
+      <div className='h-screen w-screen bg-primary'>
+        <Header />
+        <Navi />
+        <main className='w-1100 mx-auto my-2.5'>
+          <Routes>
+            <Route path='' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ client/src/containers/Public/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div className='p-[30px] pb-[100px] max-w-[600px] mx-auto my-0 bg-white border border-[#dedede] rounded-lg'>
+      <h1 className='text-[28px] font-bold mb-[15px]'>Đăng nhập</h1>
+    </div>
+  )
+}
+
+export default Login
