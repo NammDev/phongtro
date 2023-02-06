ed22a8c8a4de500110ac9d7c437fb2dc492b5e37
Nam <namdeveloper.ca@gmail.com>
2023-02-06 10:00:44 +0700

    Register & Login API

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,13 +1,16 @@
-import * as authService from '../service/auth'
+import { registereServices, loginServices } from '../service/auth'
 
-const register = async (req, res) => {
+export const register = async (req, res) => {
   const { name, phone, password } = req.body
   try {
-    if (!name || !phone || !password)
+    if (!name || !phone || !password) {
       return res.status(400).json({
         err: 1,
         msg: `Missing inputs!`,
       })
+    }
+    const response = await registereServices(req.body)
+    return res.status(200).json(response)
   } catch (error) {
     return res.status(500).json({
       err: -1,
@@ -16,4 +19,21 @@ const register = async (req, res) => {
   }
 }
 
-export default register
+export const login = async (req, res) => {
+  const { phone, password } = req.body
+  try {
+    if (!(phone && password)) {
+      return res.status(400).json({
+        err: 1,
+        msg: `Missing inputs!`,
+      })
+    }
+    const response = await loginServices(req.body)
+    return res.status(200).json(response)
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: `Failed at auth controller ${error}`,
+    })
+  }
+}
--- server/src/service/auth.js
+++ server/src/service/auth.js
@@ -1,9 +1,60 @@
 import db from '../models'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import { v4 } from 'uuid'
 
-export const registereServices = () =>
+require('dotenv').config()
+
+const hashPassword = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(12))
+
+export const registereServices = ({ phone, password, name }) =>
   new Promise(async (resolve, reject) => {
     try {
-    } catch (error) {}
+      const [row, created] = await db.User.findOrCreate({
+        where: { phone },
+        defaults: {
+          phone,
+          name,
+          password: hashPassword(password),
+          id: v4(),
+        },
+      })
+      const token =
+        created &&
+        jwt.sign({ id: row.id, phone: row.id }, process.env.SECRET_KEY, { expiresIn: '2d' })
+      resolve({
+        err: token ? 0 : 2,
+        msg: token ? 'Register is successfuly' : 'Phone number has been already in use!',
+        token: token || null,
+      })
+    } catch (error) {
+      reject(error)
+    }
+  })
+
+export const loginServices = ({ phone, password }) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.User.findOne({
+        where: { phone },
+        raw: true,
+      })
+      const isCorrectPassword = response && bcrypt.compareSync(password, response.password)
+      const token =
+        isCorrectPassword &&
+        jwt.sign({ id: response.id, phone: response.phone }, process.env.SECRET_KEY, {
+          expiresIn: '2d',
+        })
+      resolve({
+        err: token ? 0 : 2,
+        msg: token
+          ? 'Login is successfully'
+          : response
+          ? 'Wrong password'
+          : 'Phone number not found!',
+        token: token || null,
+      })
+    } catch (error) {
+      reject(error)
+    }
   })
