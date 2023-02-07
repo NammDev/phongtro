4ff390ff79ccf6ac948130236af8fe682fc03b4a
Nam <namdeveloper.ca@gmail.com>
2023-02-07 11:37:39 +0700

    Redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,13 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistor } from './store/redux'
+import { store, persistor } from './redux/store'
+import LoadingView from './components/LoadingView'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    <PersistGate loading={<LoadingView />} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
